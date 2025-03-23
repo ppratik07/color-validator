4bd24fafde255d6e2b7690a261f731f4f0f60775
pratikprajapati0708 <prajapatipratik292@gmail.com>
2025-03-23 14:34:54 +0530

    adding cors and enabling frontend

--- colorvalidatorserver/src/index.ts
+++ colorvalidatorserver/src/index.ts
@@ -1,10 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import express from "express";
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
 const prisma = new PrismaClient();
 app.use(express.json());
+app.use(cors({ origin: 'http://localhost:5173' }));
 
 //Creating new profile
 
